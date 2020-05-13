770b9deb21a57bb4032e3850cc248799ec00f535
Anthony George <ageorge530@gmail.com>
2020-05-13 07:30:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,3 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU PUMPKIN!
-elsif phrase !=phrase.upcase
-  return "HUH?! SPEAK UP, SONNY!
-else
-  return "NO, NOT SINCE 1938!"
-    
-end
-
-end
